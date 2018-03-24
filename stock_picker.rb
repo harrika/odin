5de394c549bc4efbcddfb6822e8c434d13a4dce8
harrika <henry.kirya@gmail.com>
2018-03-24 10:23:19 +0300

    adding stock_picker

---
+++ stock_picker.rb
@@ -0,0 +1,25 @@
+
+#ray = [17,3,6,9,15,8,6,1,10]
+def stock_picker(ray)
+	days = []
+	proft = 0
+	for k in 0...ray.length-1	
+		left = ray[k]
+		for i in k+1...ray.length	
+			right = ray[i]
+			if right-left > proft
+				proft = right-left
+				rr = right
+				rri = i
+				ll = left
+				lli = k
+			end
+		end
+	end	
+	days.append(lli)
+	days.append(rri)
+	return days
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
+
