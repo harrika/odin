75aa06a58271dbc9f1f89bf20c8c2364eeba4b4f
harrika <henry.kirya@gmail.com>
2018-03-30 19:07:50 +0300

    adding bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,8 +2,6 @@
 # It uses the bubble sort algorithm
 # Henry Kirya
 
-
-
 def bubble_sort(ray)
 	n = ray.length-1		
 	loop do 
@@ -26,5 +24,30 @@ ll = [4,3,78,2,0,2]
 p bubble_sort(ll)
 
 
+#bubble_sort_by uses yield to sort an array by accepting a block
+
+def bubble_sort_by(ray)
+	n = ray.length-1		
+	loop do 
+		swpd = false		
+		rr = 0.step(n-1).to_a
+		for c in rr do
+		  if yield(ray[c], ray[c+1]) > 0
+		  	ray[c], ray[c+1] = ray[c+1], ray[c]     	        	
+	 	  # if (ray[c] > ray[c+1]) 
+	 	  # 	tt = ray[c]
+	 	  # 	ray[c] = ray[c+1]
+	 	  # 	ray[c+1] = tt
+	 	  	swpd = true	 	  	
+	 	  end #end if 	 
+	 	end #end for 
+	break if swpd == false
+	end #end loop
+	p ray
+	ray
+end #end def
 
 
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+ 	left.length - right.length 	
+ end
